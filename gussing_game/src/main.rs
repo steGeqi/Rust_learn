2738962e17f6001ff02f2467cc730c194e6d0954
steGeqi <1994975650@qq.com>
2025-03-21 10:50:16 +0800

    fix:gussing_game input string will break

--- gussing_game/src/main.rs
+++ gussing_game/src/main.rs
@@ -1,7 +1,7 @@
-//use std::io;
+use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
-use rpassword::read_password;  // 秘文输入
+//use rpassword::read_password;  // 秘文输入
 // rand 库 cargo add rand@0.85
 // cargo update rand
 
@@ -11,15 +11,19 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
     println!("Please input your guess.");
     loop {
-
         // mut 可以重新赋值
-        let guess = read_password().expect("Failed to read input");
-        println!("You guessed: {}", guess);
+        //let guess = read_password().expect("Failed to read input");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-        let guess: u32 =  match guess.trim().parse()
-        {
+        println!("You guessed: {}", guess);
+        let guess: u32 =  match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Please enter a valid number.");
+                continue;
+            }
         };
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
